71383590c8ede2d778d50fdf38fb4ec89eb75dd3
uniqstha <unique2059@gmail.com>
2021-09-02 09:56:51 +0545

    Admin and employee botton is added with background

--- main.py
+++ main.py
@@ -10,18 +10,24 @@ root.resizable(0, 0)
 
 label1 = Label(root)
 label1.place(x=0, y=0, width=1366, height=768)
-
+img = PhotoImage(file="./images/loginas.png")
+label1.configure(image=img)
 
 button1 = Button(root,bg='white',fg='white',activebackground="white",relief="flat",overrelief="flat",borderwidth="0",
                  cursor='hand2')
 button1.place(x=430,y=330, width=146, height=90)
+img2 = PhotoImage(file="./images/employee_icon.png")
+button1.configure(image=img2)
 Label(root,text='EMPLOYEE',bg='white',font=('Consolas',15)).place(x=455,y=410)
 
 
 button2 = Button(root,bg='white',fg='white',activebackground="white",relief="flat",overrelief="flat",borderwidth="0",
                  cursor='hand2')
 button2.place(x=774,y=330, width=146, height=100)
+img3 = PhotoImage(file="./images/admin_icon.png")
+button2.configure(image=img3)
 Label(root,text='ADMIN',bg='white',font=('Consolas',15)).place(x=815,y=410)
 
 
 root.mainloop()
+
