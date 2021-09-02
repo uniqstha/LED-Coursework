a584b13e6b7dab663073b2925a489a9e4d3fbe69
uniqstha <unique2059@gmail.com>
2021-09-02 20:32:57 +0545

    Loginemp.py file connected with main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from tkinter import *
+import os
 #creating root window
 root = Tk()
 root.lift()
 root.geometry("1366x768+60+10")
-root.title("Employee Management System")
+root.title("Login Page")
 root.resizable(0, 0)
 
+def emp_login():
+    root.withdraw()
+    os.system("loginemp.py")
+    root.deiconify()
 
 
 label1 = Label(root)
@@ -14,7 +19,7 @@ img = PhotoImage(file="./images/loginas.png")
 label1.configure(image=img)
 
 button1 = Button(root,bg='white',fg='white',activebackground="white",relief="flat",overrelief="flat",borderwidth="0",
-                 cursor='hand2')
+                 cursor='hand2',command=emp_login)
 button1.place(x=430,y=330, width=146, height=90)
 img2 = PhotoImage(file="./images/employee_icon.png")
 button1.configure(image=img2)
